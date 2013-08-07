c43c73c9ee6b611f0103c11eb6a71dc49d8de713
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-07 10:41:13 -0500

    creating controllers and views

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,14 +1,18 @@
 #==== GETS ===========================
 get '/' do
   # Look in app/views/index.erb
+  @urls = Url.all
   erb :index
 end
 
 get '/:short_url' do
   # redirect to appropriate "long" URL
 end
+
 #==== POSTS ==========================
 
 post '/urls' do
   # create a new URL
+  Url.create(params[:long_url])
+  redirect "/"
 end
