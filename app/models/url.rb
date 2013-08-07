b4feca2362dd03fd49f97841e806d3579309a340
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-07 10:14:16 -0500

    added validations on model

--- app/models/url.rb
+++ app/models/url.rb
@@ -4,15 +4,16 @@ class Url < ActiveRecord::Base
   # Remember to create a migration!
   before_save :generate_short_url
   validates :short_url, uniqueness: true
+  validates :long_url, format: { with: /(http|https):\/{2}[a-zA-Z0-9]+.[a-zA-Z]{2,}/i }
 
-
+  
 
   private 
   def generate_short_url
 
     until self.valid?
       #generate short_url
-      self.short_url = [1,2,3].sample
+      self.short_url = SecureRandom.hex(3)
     end
 
   end
